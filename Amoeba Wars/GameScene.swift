76a507dac1d49b969f90c84905f18f8ba865b3e5
20075129 <20075129@IT103iMAC19.wit.ie>
2019-11-27 17:10:00 +0000

    Added the coins and one button to the gui, working on other two buttons now

--- Amoeba Wars/GameScene.swift
+++ Amoeba Wars/GameScene.swift
@@ -40,11 +40,47 @@ class GameScene: SKScene {
         
         // MARK: Add fowleri button
         
+        fowleriButton = ButtonNode(iconName: ImageName.FowleriLeft, text: String(GameConfig.FowleriCost), onButtonPress: fowleriPressed)
+        fowleriButton.position = CGPoint(x: size.width * 0.50, y: margin + fowleriButton.size.height / 2)
+        addChild(fowleriButton)
+        
         // MARK: Add proteus button
+        
+        proteusButton = ButtonNode(iconName: ImageName.ProteusLeft, text: String(GameConfig.ProteusCost), onButtonPress: proteusPressed)
+        proteusButton.position = CGPoint(x: size.width * 0.75, y: margin + proteusButton.size.height / 2)
+        addChild(proteusButton)
         
         // MARK: Add coin left indicator
         
+        let coinLeft = SKSpriteNode(imageNamed: ImageName.Coin)
+        
+        coinLeft.position = CGPoint(x: margin + coinLeft.size.width/2,
+                                    y: size.height - margin - coinLeft.size.height/2)
+        addChild(coinLeft)
+        coinLeftLabel.fontSize = 50
+        coinLeftLabel.fontColor = SKColor.black
+        coinLeftLabel.position = CGPoint(x: coinLeft.position.x + coinLeft.size.width/2 + margin, y: coinLeft.position.y)
+        coinLeftLabel.zPosition = Layer.HUD
+        coinLeftLabel.horizontalAlignmentMode = .left
+        coinLeftLabel.verticalAlignmentMode = .center
+        coinLeftLabel.text = "0"
+        self.addChild(coinLeftLabel)
+        
         // MARK: Add coin right indicator
+        
+        let coinRight = SKSpriteNode(imageNamed: ImageName.Coin)
+        
+        coinRight.position = CGPoint(x: size.width - margin - coinRight.size.width/2,
+                                     y: size.height - margin - coinRight.size.height/2)
+        addChild(coinRight)
+        coinRightLabel.fontSize = 50
+        coinRightLabel.fontColor = SKColor.black
+        coinRightLabel.position = CGPoint(x: coinRight.position.x - coinRight.size.width/2 - margin, y: coinRight.position.y)
+        coinRightLabel.zPosition = Layer.HUD
+        coinRightLabel.horizontalAlignmentMode = .right
+        coinRightLabel.verticalAlignmentMode = .center
+        coinRightLabel.text = "0"
+        self.addChild(coinRightLabel)
         
         // MARK: Add base left
         
@@ -59,6 +95,14 @@ class GameScene: SKScene {
     //MARK: - Button methods
     func histolyticaPressed() {
         print("Histolytica pressed!")
+    }
+    
+    func fowleriPressed() {
+        print("Fowleri pressed!")
+    }
+    
+    func proteusPressed() {
+        print("Proteus pressed!")
     }
     
 }
--- Amoeba Wars/Helpers/Constants.swift
+++ Amoeba Wars/Helpers/Constants.swift
@@ -7,11 +7,17 @@ struct ImageName {
     static let Background = "Background"
     static let Button = "Button"
     static let HistolyticaLeft = "HistolyticaLeft"
+    static let FowleriLeft = "FowleriLeft"
+    static let ProteusLeft = "ProteusLeft"
+    static let Coin = "Coin"
 }
 
 struct Layer {
     static let Background: CGFloat = 0
-    static let Button: CGFloat = 0
+    static let Button: CGFloat = 10
+    static let Left: CGFloat = 5
+    static let Right: CGFloat = 5
+    static let HUD: CGFloat = 1
 }
 
 struct PhysicsCategory {

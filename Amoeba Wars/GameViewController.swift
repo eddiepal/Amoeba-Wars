318e9a21863cafcb3a9cb6299a71322e6641eddb
20075129 <20075129@IT103iMAC19.wit.ie>
2019-11-27 15:50:25 +0000

    updated the game view controller

--- Amoeba Wars/GameViewController.swift
+++ Amoeba Wars/GameViewController.swift
@@ -1,55 +1,24 @@
-//
-//  GameViewController.swift
-//  Amoeba Wars
-//
-//  Created by 20075129 on 27/11/2019.
-//  Copyright © 2019 20075129. All rights reserved.
-//
-
 import UIKit
 import SpriteKit
-import GameplayKit
 
 class GameViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+        let aspectRatio = view.bounds.size.width / view.bounds.size.height
+        
+        let scene =
+            GameScene(size:CGSize(width: 640 * aspectRatio, height:640))
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .aspectFill
+        skView.presentScene(scene)
     }
-
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    override var prefersStatusBarHidden: Bool {
+    
+    override var prefersStatusBarHidden : Bool  {
         return true
     }
 }
